bfe7cc446e6fff0acedbfd6731b4d721195fcab5
Sahil Gupta <sahil.gupta757@gmail.com>
2024-05-12 18:11:41 +0530

    add output parser

--- ice_breaker.py
+++ ice_breaker.py
@@ -8,6 +8,8 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 
 from agents.linkedin_lookup_agent import lookup
 from third_parties.linkedin import scrape_linkedin_profile
+from output_parsers import summary_parser
+
 
 def ice_break_with(name: str) -> str:
     """
@@ -21,18 +23,23 @@ def ice_break_with(name: str) -> str:
         given the information {information} about a person I want you to create:
         1. a short summary
         2. two interesting facts about them
+        \n{format_instructions}
         """
 
     summary_prompt_template = PromptTemplate(
-        input_variables=["information"], template=summary_template
+        input_variables=["information"],
+        template=summary_template,
+        partial_variables={
+            "format_instructions": summary_parser.get_format_instructions()
+        },
     )
 
     llm = ChatGoogleGenerativeAI(temperature=0, model="gemini-1.0-pro")
 
-    chain = LLMChain(llm=llm, prompt=summary_prompt_template)
+    # chain = LLMChain(llm=llm, prompt=summary_prompt_template)
+    chain = summary_prompt_template | llm | summary_parser
     res = chain.invoke(input={"information": linkedin_data})
     pprint(res)
-
 
 
 if __name__ == "__main__":
